8b479a7d83df0091b43ca924872a0e3a3eb4f366
Florence-Njeri <flonsie17@gmail.com>
2020-06-13 21:24:41 +0300

    add dark mode support

--- Week3/app/src/main/java/com/florencenjeri/businesscard/MainActivity.kt
+++ Week3/app/src/main/java/com/florencenjeri/businesscard/MainActivity.kt
@@ -2,10 +2,30 @@ package com.florencenjeri.businesscard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    enum class DarkModeConfig{
+        YES,
+        NO,
+        FOLLOW_SYSTEM
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonMode.setOnClickListener {
+            shouldEnableDarkMode(DarkModeConfig.NO)
+
+        }
     }
+
+    fun shouldEnableDarkMode(darkModeConfig: DarkModeConfig){
+        when(darkModeConfig){
+            DarkModeConfig.YES -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            DarkModeConfig.NO -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            DarkModeConfig.FOLLOW_SYSTEM -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+        }
+    }
+
 }
