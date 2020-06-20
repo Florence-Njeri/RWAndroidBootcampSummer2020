b588ad921d52427e13e038e8d0bda09922a60346
Florence-Njeri <flonsie17@gmail.com>
2020-06-20 18:44:04 +0300

    feat : add RecyclerView Adapter and populate the dummy data to the adapter

--- Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/BooksAdapter.kt
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/BooksAdapter.kt
@@ -1,4 +1,59 @@
 package com.florencenjeri.readinglist
 
-class BooksAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.florencenjeri.readinglist.model.Books
+
+class BooksAdapter(private val books: Array<Books>) :
+    RecyclerView.Adapter<BooksAdapter.BooksViewHolder>() {
+
+    class BooksViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
+
+        private var bookTitle: TextView = view.findViewById(R.id.book_title)
+        private var author: TextView = view.findViewById(R.id.author)
+        private var publicationDate: TextView = view.findViewById(R.id.publication_date)
+        private var pages: TextView = view.findViewById(R.id.num_pages)
+        private var genre: TextView = view.findViewById(R.id.genre)
+        private var bookCover: ImageView = view.findViewById(R.id.cover)
+
+        init {
+            view.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            v.findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        fun bindPhoto(books: Books) {
+            //TODO: Use Glide for book cover  image loading
+
+            bookTitle.text = books.title
+            author.text = books.author.name
+            publicationDate.text = books.publicationDate
+            pages.text = books.pages
+            genre.text = books.genre
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BooksViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.book_list_item, parent, false)
+        return BooksViewHolder(view)
+
+    }
+
+    override fun getItemCount() = books.size
+
+    override fun onBindViewHolder(holder: BooksViewHolder, position: Int) {
+        //Show the right view depending on the users position in scrolling
+        val itemBook = books[position]
+        holder.bindPhoto(itemBook)
+
+    }
 }
--- Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/BooksFragment.kt
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/BooksFragment.kt
@@ -7,15 +7,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.florencenjeri.readinglist.model.BooksData
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class BooksFragment : Fragment() {
-
+    val booksList = BooksData.booksRead
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.books_fragment, container, false)
@@ -23,7 +25,8 @@ class BooksFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        val bookRecyclerView = view.findViewById<RecyclerView>(R.id.books_list)
+        bookRecyclerView.adapter = BooksAdapter(booksList.toTypedArray())
 
     }
 }
