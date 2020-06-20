ab8d24fb3196643d404dd80970ea48313769a09a
Florence-Njeri <flonsie17@gmail.com>
2020-06-20 08:36:24 +0300

    feat: add Books.kt and Author.kt data classes

---
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/Author.kt
@@ -0,0 +1,3 @@
+package com.florencenjeri.readinglist.model
+
+data class Author(val name: String, val origin:String)
---
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/Books.kt
@@ -0,0 +1,3 @@
+package com.florencenjeri.readinglist.model
+//TODO: add AUTHOR object
+data class Books(val image:Int, val title:String, val author:Author, val publicationDate:String, val synopsis:String,val genre:String)
