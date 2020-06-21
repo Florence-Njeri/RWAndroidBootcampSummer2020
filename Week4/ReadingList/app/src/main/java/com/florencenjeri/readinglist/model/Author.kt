2cccf7f31d3d986cbeb6d545c8a21a03f065cc3b
Florence-Njeri <flonsie17@gmail.com>
2020-06-21 09:20:29 +0300

    feat : implement Parcelable in the data models

--- Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/Author.kt
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/Author.kt
@@ -1,3 +1,7 @@
 package com.florencenjeri.readinglist.model
 
-data class Author(val name: String, val nationality:String)
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Author(val name: String, val nationality:String):Parcelable
--- Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/Books.kt
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/Books.kt
@@ -1,3 +1,15 @@
 package com.florencenjeri.readinglist.model
-//TODO: add AUTHOR object
-data class Books(val image:Int, val title:String, val author:Author, val publicationDate:String, val pages:String,val synopsis:String,val genre:String)
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Books(
+    val image: Int,
+    val title: String,
+    val author: Author,
+    val publicationDate: String,
+    val pages: String,
+    val synopsis: String,
+    val genre: String
+) : Parcelable
