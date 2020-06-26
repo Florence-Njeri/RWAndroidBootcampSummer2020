e0b7629106ce06bfa0fd8588e4aa7a860073c0ae
Florence-Njeri <flonsie17@gmail.com>
2020-06-26 22:12:33 +0300

    add: sharedPrefs for the user

---
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/ReadingListApplication.kt
@@ -0,0 +1,18 @@
+package com.florencenjeri.readinglist
+
+import android.app.Application
+import android.content.Context
+
+class ReadingListApplication : Application() {
+
+    companion object {
+        private lateinit var instance: ReadingListApplication
+
+        fun getAppContext(): Context = instance.applicationContext
+    }
+
+    override fun onCreate() {
+        instance = this
+        super.onCreate()
+    }
+}
---
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/UserPrefs.kt
@@ -0,0 +1,20 @@
+package com.florencenjeri.readinglist.model
+
+import android.preference.PreferenceManager
+import com.florencenjeri.readinglist.ReadingListApplication
+
+object UserPrefs {
+    private const val EMAIL = "email"
+    private const val PASSWORD = "password"
+
+    private fun sharedPrefs() =
+        PreferenceManager.getDefaultSharedPreferences(ReadingListApplication.getAppContext())
+
+    fun userIsLoggedIn(email: String, password: String) {
+        val editor = sharedPrefs().edit()
+        editor.putString(EMAIL, email)
+        editor.putString(PASSWORD, password)
+    }
+
+    fun getUserCredentials(): String = sharedPrefs().getString(EMAIL, "flonjeri@mail.com")!!
+}
