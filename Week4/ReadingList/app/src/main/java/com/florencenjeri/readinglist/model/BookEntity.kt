37c703b5c3d6d144b8684a281f5059adcd8b5b21
Florence-Njeri <flonsie17@gmail.com>
2020-06-28 19:08:01 +0300

    add: room dependencies and BooksEntity object

---
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/model/BookEntity.kt
@@ -0,0 +1,25 @@
+package com.florencenjeri.readinglist.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "reading_list")
+data class BookEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0,
+    @ColumnInfo(name = "image")
+    val image: Int,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "author")
+    val author: Author,
+    @ColumnInfo(name = "publicationDate")
+    val publicationDate: String,
+    @ColumnInfo(name = "pages")
+    val pages: String,
+    @ColumnInfo(name = "synopsis")
+    val synopsis: String,
+    @ColumnInfo(name = "genre")
+    val genre: String
+)
