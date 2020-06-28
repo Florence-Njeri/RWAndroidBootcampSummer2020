9f86dd23e87845e6694242fe9f58e0921e974289
Florence-Njeri <flonsie17@gmail.com>
2020-06-28 19:44:09 +0300

    create: an instance of the room db in the Application() class

--- Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/ReadingListApplication.kt
+++ Week4/ReadingList/app/src/main/java/com/florencenjeri/readinglist/ReadingListApplication.kt
@@ -2,17 +2,24 @@ package com.florencenjeri.readinglist
 
 import android.app.Application
 import android.content.Context
+import androidx.room.Room
+import com.florencenjeri.readinglist.model.BooksDatabase
 
 class ReadingListApplication : Application() {
 
     companion object {
         private lateinit var instance: ReadingListApplication
 
+        //Using a singleton pattern sing Room is resource intensive
+        var database: BooksDatabase? = null
+
         fun getAppContext(): Context = instance.applicationContext
     }
 
     override fun onCreate() {
         instance = this
+        database =
+            Room.databaseBuilder(this, BooksDatabase::class.java, "reading-list-db").build()
         super.onCreate()
     }
 }
