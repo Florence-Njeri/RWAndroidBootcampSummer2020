ecc94ad10bfdeb101e2e5540b9cc99062d74b391
Florence-Njeri <flonsie17@gmail.com>
2020-07-13 22:57:39 +0300

    add string formatter to news_item layout

--- Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/ui/NewsViewHolder.kt
+++ Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/ui/NewsViewHolder.kt
@@ -3,6 +3,8 @@ package com.florencenjeri.cocktailsrecipe.ui
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.florencenjeri.cocktailsrecipe.App
+import com.florencenjeri.cocktailsrecipe.R
 import com.florencenjeri.cocktailsrecipe.model.New
 import kotlinx.android.synthetic.main.news_item.view.*
 import java.text.SimpleDateFormat
@@ -11,15 +13,16 @@ import java.util.*
 class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     fun setBookData(new: New) {
-
+        val context = App.getAppContext()
         itemView.title.text = new.title
-        itemView.author.text = new.author
+        itemView.author.text = context.getString(R.string.author,new.author)
         val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z", Locale.getDefault())
         val date = dateFormat.parse(new.published)
-        val formatter = SimpleDateFormat("yyyy-MM-dd") //If you need time just put specific format for time like ‘HH:mm:ss’
+        val formatter =
+            SimpleDateFormat("yyyy-MM-dd") //If you need time just put specific format for time like ‘HH:mm:ss’
         val dateStr = formatter.format(date)
-        itemView.publicationDate.text = dateStr
-        itemView.language.text = new.language
+        itemView.publicationDate.text =context.getString(R.string.publication_date, dateStr)
+        itemView.language.text = context.getString(R.string.language,new.language)
         Glide.with(itemView)
             .load(new.image)
             .centerCrop()
