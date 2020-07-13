ac6997231ffcf5d7fa90edad687bf3b1ab9e8edf
Florence-Njeri <flonsie17@gmail.com>
2020-07-13 08:39:58 +0300

    add room database functionality

--- Week7/CocktailsRecipe/app/src/main/java/com/florencenjeri/cocktailsrecipe/model/database/NewsDatabase.kt
+++ Week7/CocktailsRecipe/app/src/main/java/com/florencenjeri/cocktailsrecipe/model/database/NewsDatabase.kt
@@ -1,5 +1,44 @@
 package com.florencenjeri.cocktailsrecipe.model.database
 
-abstract class NewsDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.florencenjeri.cocktailsrecipe.model.LatestNews
+import kotlinx.coroutines.CoroutineScope
+
+@Database(entities = [(LatestNews::class)], version = 1)
+abstract class NewsDatabase : RoomDatabase() {
     abstract fun newsDao(): NewsDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: NewsDatabase? = null
+
+        fun getDatabase(
+            context: Context,
+            scope: CoroutineScope
+        ): NewsDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NewsDatabase::class.java,
+                    NEWS_DB
+
+                )
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+
+        const val NEWS_DB = "news-db"
+    }
+
 }
