462108f743532f9db3af56db66f104295c5e1f62
Florence-Njeri <flonsie17@gmail.com>
2020-07-13 22:43:15 +0300

    add Date formatter

--- Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/ui/NewsViewHolder.kt
+++ Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/ui/NewsViewHolder.kt
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.florencenjeri.cocktailsrecipe.model.New
 import kotlinx.android.synthetic.main.news_item.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
@@ -12,7 +14,11 @@ class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         itemView.title.text = new.title
         itemView.author.text = new.author
-        itemView.publicationDate.text = new.published
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z", Locale.getDefault())
+        val date = dateFormat.parse(new.published)
+        val formatter = SimpleDateFormat("yyyy-MM-dd") //If you need time just put specific format for time like ‘HH:mm:ss’
+        val dateStr = formatter.format(date)
+        itemView.publicationDate.text = dateStr
         itemView.language.text = new.language
         Glide.with(itemView)
             .load(new.image)
