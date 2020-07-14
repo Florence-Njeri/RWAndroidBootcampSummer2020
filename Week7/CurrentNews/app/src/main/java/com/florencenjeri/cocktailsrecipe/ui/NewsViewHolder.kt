099b4e7c3cadaeac267636b2f1c445bed12ae675
Florence-Njeri <flonsie17@gmail.com>
2020-07-14 20:17:15 +0300

    modify glide

--- Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/ui/NewsViewHolder.kt
+++ Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/ui/NewsViewHolder.kt
@@ -15,17 +15,20 @@ class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun setBookData(new: New) {
         val context = App.getAppContext()
         itemView.title.text = new.title
-        itemView.author.text = context.getString(R.string.author,new.author)
+        itemView.author.text = context.getString(R.string.author, new.author)
         val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z", Locale.getDefault())
         val date = dateFormat.parse(new.published)
         val formatter =
             SimpleDateFormat("yyyy-MM-dd") //If you need time just put specific format for time like ‘HH:mm:ss’
         val dateStr = formatter.format(date)
-        itemView.publicationDate.text =context.getString(R.string.publication_date, dateStr)
-        itemView.language.text = context.getString(R.string.language,new.language)
+        itemView.publicationDate.text = context.getString(R.string.publication_date, dateStr)
+        itemView.language.text = context.getString(R.string.language, new.language)
         Glide.with(itemView)
             .load(new.image)
             .centerCrop()
+            .placeholder(R.drawable.ic_image_place_holder) //5
+            .error(R.drawable.ic_broken_image) //6
+            .fallback(R.drawable.ic_no_image) //7
             .into(itemView.image)
     }
 }
