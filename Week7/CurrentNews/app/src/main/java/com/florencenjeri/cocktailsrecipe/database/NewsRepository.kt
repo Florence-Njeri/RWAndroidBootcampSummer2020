59764216e0338c986337d3d2c145ffc5517249a2
Florence-Njeri <flonsie17@gmail.com>
2020-07-20 07:37:00 +0300

    refresh Repository data once the user is connected to the internet

--- Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/database/NewsRepository.kt
+++ Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/database/NewsRepository.kt
@@ -1,19 +1,26 @@
 package com.florencenjeri.cocktailsrecipe.database
 
+import android.net.ConnectivityManager
 import androidx.lifecycle.LiveData
 import com.florencenjeri.cocktailsrecipe.App
 import com.florencenjeri.cocktailsrecipe.model.News
 import com.florencenjeri.cocktailsrecipe.model.Success
+import com.florencenjeri.cocktailsrecipe.network.NetworkStatusChecker
 
 class NewsRepository() {
+    private val networkStatusChecker by lazy {
+        NetworkStatusChecker(App.getAppContext().getSystemService(ConnectivityManager::class.java))
+    }
+
     fun fetchNews(): LiveData<List<News>> = App.newsDao.fetchNews()
 
     suspend fun insertNews() {
         //Refresh the database data once a user is connected to the internet
-
-        val result = App.remoteApi.fetchNews()
-        if (result is Success) {
-            return App.newsDao.insertNews(result.data)
+        networkStatusChecker.performIfConnectedToInternet {
+            val result = App.remoteApi.fetchNews()
+            if (result is Success) {
+                return App.newsDao.insertNews(result.data)
+            }
         }
 
     }
