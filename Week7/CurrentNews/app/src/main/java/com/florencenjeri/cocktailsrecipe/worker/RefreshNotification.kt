578735ab610a6af24b8b0bd09ec42e9aed77e96f
Florence-Njeri <flonsie17@gmail.com>
2020-07-21 16:07:40 +0300

    add a pending Intent to the notification so on click it launches the application

--- Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/worker/RefreshNotification.kt
+++ Week7/CurrentNews/app/src/main/java/com/florencenjeri/cocktailsrecipe/worker/RefreshNotification.kt
@@ -1,12 +1,23 @@
 package com.florencenjeri.cocktailsrecipe.worker
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import com.florencenjeri.cocktailsrecipe.R
+import com.florencenjeri.cocktailsrecipe.ui.MainActivity
 
 private val NOTIFICATION_ID = 0
 fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+
+    val contentIntent = Intent(applicationContext, MainActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
     val builder = NotificationCompat.Builder(
         applicationContext,
         applicationContext.getString(R.string.refresh_data_channel_id)
@@ -14,5 +25,7 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
         .setSmallIcon(R.drawable.ic_baseline_new_releases_24)
         .setContentTitle(applicationContext.getString(R.string.notification_title))
         .setContentText(messageBody)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
     notify(NOTIFICATION_ID, builder.build())
 }
