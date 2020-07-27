0b3ac935d230f2ea06aa4a5a180bf1e114cef413
Florence-Njeri <flonsie17@gmail.com>
2020-07-27 17:31:50 +0300

    resolve merge conflicts

--- Week7/CurrentNews/app/src/main/java/com/florencenjeri/currentnews/ui/viewmodel/NewsViewModel.kt
+++ Week7/CurrentNews/app/src/main/java/com/florencenjeri/currentnews/ui/viewmodel/NewsViewModel.kt
@@ -1,16 +1,14 @@
 package com.florencenjeri.currentnews.ui.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.florencenjeri.currentnews.database.NewsRepository
-import com.florencenjeri.currentnews.model.News
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class NewsViewModel(val newsRepository: NewsRepository) : ViewModel() {
 
-    fun fetchNews():LiveData<List<News>> = newsRepository.fetchNews()
+    fun fetchNews() = newsRepository.fetchNews()
 
     fun refreshNewsInDb() = viewModelScope.launch(Dispatchers.IO) {
         newsRepository.refreshNews()
