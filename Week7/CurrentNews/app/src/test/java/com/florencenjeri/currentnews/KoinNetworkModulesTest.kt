d0c53fbf8f9cd8c7060e75a320a818219f70c5d8
Florence-Njeri <flonsie17@gmail.com>
2020-08-01 22:18:10 +0300

    fix D.I bug caused by lack of applicationContext in the UnitTest

--- Week7/CurrentNews/app/src/test/java/com/florencenjeri/currentnews/KoinNetworkModulesTest.kt
+++ Week7/CurrentNews/app/src/test/java/com/florencenjeri/currentnews/KoinNetworkModulesTest.kt
@@ -1,19 +1,32 @@
 package com.florencenjeri.currentnews
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import com.florencenjeri.currentnews.di.networkModule
 import com.florencenjeri.currentnews.di.newsModule
 import org.junit.After
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.test.KoinTest
 import org.koin.test.check.checkModules
+import org.robolectric.RobolectricTestRunner
 
+
+@RunWith(RobolectricTestRunner::class)
 class KoinModulesTest : KoinTest {
+    val appContext = ApplicationProvider.getApplicationContext<Context>()
+
     @Test
     fun `test koin modules`() {
         startKoin {
-
+            //For logging Koin related errors
+            androidLogger()
+            //Declare my app context
+            androidContext(appContext)
             modules(listOf(newsModule, networkModule))
         }.checkModules()
     }
